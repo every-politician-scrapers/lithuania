96dd9ccaf1516aaf66252eb518037c4a31774515
Tony Bowden <tony@tmtm.com>
2021-08-07 11:03:06 +0300

    Update to every-politician-scraper 0.0.24

--- create-office.js
+++ create-office.js
@@ -10,18 +10,16 @@ module.exports = (label) => {
       en: label,
     },
     descriptions: {
-      en: 'Lithuanian cabinet position',
+      en: `cabinet position in ${meta.jurisdiction.name}`,
     },
     claims: {
       P31:   { value: 'Q294414' }, // instance of: public office
       P279:  { value: 'Q83307'  }, // subclas of: minister
-      P17:   { value: 'Q37'    },  // country: Lithuania
-      P1001: { value: 'Q37'     }, // jurisdiction: Lithuania
-      P361: {
-        value: 'Q29078891',        // part of: Cabinet of Lithuania
-        references: {
-          P854: meta.source.url
-        },
+      P17:   { value: meta.country ? meta.country.id : meta.jurisdiction.id },
+      P1001: { value: meta.jurisdiction.id },
+      P361: { // part of
+        value: meta.cabinet.parent,
+        references: { P854: meta.source.url },
       }
     }
   }
