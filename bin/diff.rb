ca40b81c996b7eeb22fbc3be10842870109ab149
Tony Bowden <tony@tmtm.com>
2022-05-24 13:08:47 +0300

    Switch to nullless diffs

--- bin/diff.rb
+++ bin/diff.rb
@@ -8,6 +8,6 @@ SKIP = [
   ['---', 'Ingrida Šimonytė', 'Prime Minister of Lithuania']
 ].freeze
 
-diff = EveryPoliticianScraper::Comparison.new('data/wikidata.csv', 'data/official.csv').diff
+diff = EveryPoliticianScraper::NulllessComparison.new('data/wikidata.csv', 'data/official.csv').diff
                                          .reject { |row| SKIP.include? row }
 puts diff.sort_by { |r| [r.first, r.last.to_s] }.reverse.map(&:to_csv)
