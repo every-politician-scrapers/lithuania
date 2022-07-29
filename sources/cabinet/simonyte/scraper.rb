0eef5a17ac98a8979f530a0a4bfc69c0892e9856
Tony Bowden <tony@tmtm.com>
2022-07-29 10:03:41 +0300

    compare enwiki Šimonytė Cabinet

---
+++ sources/cabinet/simonyte/scraper.rb
@@ -0,0 +1,58 @@
+#!/bin/env ruby
+# frozen_string_literal: true
+
+require 'every_politician_scraper/scraper_data'
+require 'pry'
+
+class MemberList
+  class Members
+    decorator RemoveReferences
+    decorator UnspanAllTables
+    decorator WikidataIdsDecorator::Links
+
+    def member_container
+      noko.xpath("//table[.//th[contains(.,'Position')]]//tr[td]")
+    end
+  end
+
+  class Member
+    field :id do
+      name_node.css('a/@wikidata').first
+    end
+
+    field :name do
+      name_node.text.tidy
+    end
+
+    field :positionID do
+    end
+
+    field :position do
+      tds[0].text.tidy
+    end
+
+    field :startDate do
+      WikipediaDate.new(raw_start).to_s
+    end
+
+    field :endDate do
+    end
+
+    private
+
+    def tds
+      noko.css('td')
+    end
+
+    def name_node
+      tds[1]
+    end
+
+    def raw_start
+      tds[4].text.tidy
+    end
+  end
+end
+
+url = ARGV.first
+puts EveryPoliticianScraper::ScraperData.new(url).csv
